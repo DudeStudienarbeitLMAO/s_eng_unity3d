0270a57aab19b3b8837bbfec81ab07eb38c808aa
WowItsSoftwareEngineering <lordluegen@gmail.com>
2017-06-14 01:04:02 +0200

    I made more things

---
+++ s_eng_unity3d/Assets/MouseLook.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour {
+
+	public enum RotationAxes {
+		MouseXandY=0,
+		MouseX = 1,
+		MouseY = 2
+	
+	}
+	public RotationAxes axes = RotationAxes.MouseXandY;
+	public float sensitivityHorz = 8.0f;
+	public float sensitivityVert = 8.0f;
+	public float minimumView = -45.0f;
+	public float maximumView = 45.0f;
+
+	private float _rotation = 0;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (axes == RotationAxes.MouseX) {
+			transform.Rotate (0, Input.GetAxis ("Mouse X") * sensitivityHorz, 0);
+		} else if (axes == RotationAxes.MouseY) {
+			_rotation -= Input.GetAxis ("Mouse Y") * sensitivityVert;
+			_rotation = Mathf.Clamp (_rotation, minimumView, maximumView);
+
+			float rotationY = transform.localEulerAngles.y;
+
+			transform.localEulerAngles = new Vector3 (_rotation, rotationY, 0);
+		} else {
+			_rotation -= Input.GetAxis ("Mouse Y") * sensitivityVert;
+			_rotation = Mathf.Clamp (_rotation, minimumView, maximumView);
+
+			float delta = Input.GetAxis ("Mouse X") * sensitivityHorz;
+			float rotationYDelta = transform.localEulerAngles.y + delta;
+
+			transform.localEulerAngles = new Vector3 (_rotation, rotationYDelta, 0);
+
+
+		
+		
+		}
+	}
+}
---
+++ s_eng_unity3d/Assets/Movement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+	public float speed = 2.0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float deltaX = Input.GetAxis ("Horizontal") * speed;
+		float deltaZ = Input.GetAxis ("Vertical") * speed;
+		transform.Translate (deltaX, 0, deltaZ);
+	}
+}
